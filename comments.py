edee8141392b76b9cdd3b29edd17363cc0bb4cac
limbero <samuelsson.axel@gmail.com>
2015-10-09 10:31:10 +0200

    User agent conforming to reddit guidelines.

--- comments.py
+++ comments.py
@@ -1,5 +1,5 @@
 import praw
-r = praw.Reddit('KTH AI project by group-55.')
+r = praw.Reddit('macosx:kth-ai-group55:v1.0 (by /u/limbero)')
 submission = r.get_submission(submission_id='3ny9fl')
 flat_comments = praw.helpers.flatten_tree(submission.comments)
 for comment in flat_comments:
