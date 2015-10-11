3138165496d2c03e4e031d26faeb4b109d39182c
limbero <samuelsson.axel@gmail.com>
2015-10-11 16:56:59 +0200

    Fetch all comments.

--- comments.py
+++ comments.py
@@ -5,6 +5,7 @@ r = praw.Reddit('macosx:kth-ai-group55:v1.0 (by /u/limbero)')
 submissions = r.get_subreddit(sys.argv[1]).get_top(limit=sys.argv[2])
 
 for submission in submissions:
+    submission.replace_more_comments(limit=None, threshold=0)
     flat_comments = praw.helpers.flatten_tree(submission.comments)
     for comment in flat_comments:
         print comment.body.encode("utf-8")
