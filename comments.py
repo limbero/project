c305cc7dd37599548fcd95da543341df73085b46
limbero <samuelsson.axel@gmail.com>
2015-10-11 19:30:37 +0200

    Limit to 100.

--- comments.py
+++ comments.py
@@ -2,7 +2,7 @@ import sys, praw
 
 r = praw.Reddit('macosx:kth-ai-group55:v1.0 (by /u/limbero)')
 
-submissions = r.get_subreddit(sys.argv[1]).get_top_from_all(limit=sys.argv[2])
+submissions = r.get_subreddit(sys.argv[1]).get_top_from_all(limit=100)
 
 for submission in submissions:
     submission.replace_more_comments(limit=None, threshold=0)
