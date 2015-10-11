ce2f9d2b7d7855634b1aacf410b114d3594b1d45
limbero <samuelsson.axel@gmail.com>
2015-10-11 16:53:46 +0200

    Ready for corpus generation.

--- comments.py
+++ comments.py
@@ -1,6 +1,10 @@
-import praw
+import sys, praw
+
 r = praw.Reddit('macosx:kth-ai-group55:v1.0 (by /u/limbero)')
-submission = r.get_submission(submission_id='3ny9fl')
-flat_comments = praw.helpers.flatten_tree(submission.comments)
-for comment in flat_comments:
-    print comment.body
+
+submissions = r.get_subreddit(sys.argv[1]).get_top(limit=sys.argv[2])
+
+for submission in submissions:
+    flat_comments = praw.helpers.flatten_tree(submission.comments)
+    for comment in flat_comments:
+        print comment.body.encode("utf-8")
